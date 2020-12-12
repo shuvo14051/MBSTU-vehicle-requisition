12f7dd631bb6ff0eecd6f060a67de26f2149824b
shuvo14051 <shuvo1137017@gmail.com>
2020-12-13 02:15:13 +0600

    MBSTU Vehicle

---
+++ index.php
@@ -0,0 +1,135 @@
+<?php 
+session_start();
+include('includes/config.php');
+error_reporting(0);
+
+?>
+
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width,initial-scale=1">
+<meta name="keywords" content="">
+<meta name="description" content="">
+<title>MBSTU Car Requisition</title>
+<!--Bootstrap -->
+<link rel="stylesheet" href="assets/css/bootstrap.min.css" type="text/css">
+<link rel="stylesheet" href="assets/css/style.css" type="text/css">
+<link rel="stylesheet" href="assets/css/owl.carousel.css" type="text/css">
+<link rel="stylesheet" href="assets/css/owl.transitions.css" type="text/css">
+<link href="assets/css/slick.css" rel="stylesheet">
+<link href="assets/css/bootstrap-slider.min.css" rel="stylesheet">
+<link href="assets/css/font-awesome.min.css" rel="stylesheet">
+		<link rel="stylesheet" id="switcher-css" type="text/css" href="assets/switcher/css/switcher.css" media="all" />
+		<link rel="alternate stylesheet" type="text/css" href="assets/switcher/css/red.css" title="red" media="all" data-default-color="true" />
+		<link rel="alternate stylesheet" type="text/css" href="assets/switcher/css/orange.css" title="orange" media="all" />
+		<link rel="alternate stylesheet" type="text/css" href="assets/switcher/css/blue.css" title="blue" media="all" />
+		<link rel="alternate stylesheet" type="text/css" href="assets/switcher/css/pink.css" title="pink" media="all" />
+		<link rel="alternate stylesheet" type="text/css" href="assets/switcher/css/green.css" title="green" media="all" />
+		<link rel="alternate stylesheet" type="text/css" href="assets/switcher/css/purple.css" title="purple" media="all" />
+<link rel="apple-touch-icon-precomposed" sizes="144x144" href="assets/images/favicon-icon/apple-touch-icon-144-precomposed.png">
+<link rel="apple-touch-icon-precomposed" sizes="114x114" href="assets/images/favicon-icon/apple-touch-icon-114-precomposed.html">
+<link rel="apple-touch-icon-precomposed" sizes="72x72" href="assets/images/favicon-icon/apple-touch-icon-72-precomposed.png">
+<link rel="apple-touch-icon-precomposed" href="assets/images/favicon-icon/apple-touch-icon-57-precomposed.png">
+<link rel="shortcut icon" href="assets/images/favicon-icon/logo.png">
+<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,900" rel="stylesheet"> 
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lobster&effect=brick-sign">
+
+</head>
+<body>
+
+<!-- Start Switcher -->
+<!-- /Switcher -->  
+        
+<!--Header-->
+<?php include('includes/header.php');?>
+<!-- /Header --> 
+
+<!-- Banners -->
+<section id="banner" class="banner-section">
+  <div class="container">
+    <div class="div_zindex">
+      <div class="row">
+        <div class="col-md-3 col-md-push-9">
+          <div class="banner_content">
+            <h1 style="font-family: Lobster;" >MBSTU  
+            vehicle requisition</h1>
+            <a href="#v" class="btn">Read More <span class="angle_arrow"><i class="fa fa-angle-right" aria-hidden="true"></i></span></a> </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<!-- /Banners --> 
+
+
+<!-- Resent Cat-->
+<section class="section-padding gray-bg">
+  <div class="container">
+    <div class="section-header" id="v">
+      <h2 class=" text-center">About MBSTU Vehicle Requisition</h2>
+      <p  class="vsystem vsystem1 ">
+         That is why the students of MBSTU are well prepared to face the challenges of day to day life anywhere.
+        MBSTU leads a lifestyle which is colorful, exciting & full of various interesting events. The students are ought to attend their classes from 8:00a.m to 5:00 p.m during the weekdays - Saturday to Wednesday. Thursday and Friday are weekend. The students get the opportunity to visit various interesting places as educational tours every now and then.The divine power lies in the understanding of science and the application of technology.  MawlanaBhashani Science and Technology University is aimed to provide its students with the knowledge of proper combination of science and technology to get them prepared for the challenges of the 21st century.
+
+      </p>
+
+      <p  class="vsystem vsystem1 ">
+        The students of MBSTU hold not only the scientific knowledge but also the norm and values to become complete social human beings who would break all the obstacles of the society and lead it to a better one as they are influenced by the vision of the spiritual philosopher and the leader of the leaders - Mawlana Abdul Hamid Khan Bhashani.
+      </p>
+      <br>
+      <p class="vsystem vsystem2 text-center">
+        The Chancellor of the University <br />
+        Md. Abdul Hamid <br />
+        President, People’s Republic of Bangladesh <br /> <br />
+
+        The Vice-Chancellor <br />
+        Professor Dr. Md. Alauddin <br />
+        </p>
+    </div>
+  
+</section>
+
+
+
+<!--Footer -->
+<?php include('includes/footer.php');?>
+<!-- /Footer--> 
+
+<!--Back to top-->
+<div id="back-top" class="back-top"> <a href="#top"><i class="fa fa-angle-up" aria-hidden="true"></i> </a> </div>
+<!--/Back to top--> 
+
+<!--Login-Form -->
+<?php include('includes/login.php');?>
+<!--/Login-Form --> 
+
+<!--Register-Form -->
+<?php include('includes/registration.php');?>
+
+<!--/Register-Form --> 
+
+<!--Forgot-password-Form -->
+<?php include('includes/forgotpassword.php');?>
+<!--/Forgot-password-Form --> 
+
+<!-- Scripts --> 
+<script src="assets/js/jquery.min.js"></script>
+<script src="assets/js/bootstrap.min.js"></script> 
+<script src="assets/js/interface.js"></script> 
+<!--Switcher-->
+<script src="assets/switcher/js/switcher.js"></script>
+<!--bootstrap-slider-JS--> 
+<script src="assets/js/bootstrap-slider.min.js"></script> 
+<!--Slider-JS--> 
+<script src="assets/js/slick.min.js"></script> 
+<script src="assets/js/owl.carousel.min.js"></script>
+
+  <script src="assets/js/navbar-fixed.js"></script>
+
+</body>
+
+
+</html>
